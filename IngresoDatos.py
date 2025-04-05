7a3386b93b0e085e8ee65ed826fd76aff19842e0
Frankyoel <Frankyoel17@gmail.com>
2025-04-04 19:42:05 -0500

    Primera actualizacion

---
+++ IngresoDatos.py
@@ -0,0 +1,6 @@
+print("¿Como te llamas?")
+n = input()
+print("Hola amigo", n)
+
+m = input("Estas listo si o no \n" )
+print("Responde",n)
---
+++ msj.py
@@ -0,0 +1,18 @@
+print("Vamos a programar"),
+
+#Bienvenido
+a = 2 **4
+name = "Y demas gente"
+b = "Hola "  "Amigos del Youtube "  + name
+
+#Aqui ya no se usa el " + " para agregarle los datos, solo se usa la coma " , "
+
+
+print(a , b)
+print(b)
+
+print("Steve", "Juan", "Señor", sep=" | ")
+print("Steve", "Juan", "Señor", sep=" | ", end = "# \n")
+
+d = f"Bienvenido amigo {name} tu puedes "
+print(d)
